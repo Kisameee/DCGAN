bf59ded47a07c2a0960984cd9eba305e55533758
Kisameee <cherfaoui.ghiles@gmail.com>
2019-06-30 16:34:18 +0200

    adding combined images

--- dcgan.py
+++ dcgan.py
@@ -1,3 +1,5 @@
+import math
+import numpy as np
 from keras.layers import Dense
 from keras.layers import Reshape
 from keras.layers.convolutional import Conv2D, MaxPooling2D
@@ -43,3 +45,26 @@ def discriminator_model():
     model.add(Dense(1))
     model.add(Activation('sigmoid'))
     return model
+
+
+def generator_containing_discriminator(g, d):
+    model = Sequential()
+    model.add(g)
+    d.trainable = False
+    model.add(d)
+    return model
+
+
+def combine_images(generated_images):
+    num = generated_images.shape[0]
+    width = int(math.sqrt(num))
+    height = int(math.ceil(float(num) / width))
+    shape = generated_images.shape[1:3]
+    image = np.zeros((height * shape[0], width * shape[1]),
+                     dtype=generated_images.dtype)
+    for index, img in enumerate(generated_images):
+        i = int(index / width)
+        j = index % width
+        image[i * shape[0]:(i + 1) * shape[0], j * shape[1]:(j + 1) * shape[1]] = \
+            img[:, :, 0]
+    return image
